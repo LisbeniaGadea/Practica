69bab1fd41031b471b120235cb22c66b3b9274ef
LisbeniaGadea <lisbeniagad@gmail.com>
2025-03-28 17:19:20 -0600

    subir

--- js/scriptss.js
+++ js/scriptss.js
@@ -1,12 +1,52 @@
-function saludar() {
-    alert("Registrese");
-}
-function saludar2() {
-    alert("Registro enviado");
-}
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("mi_boton").addEventListener("click", function() {
-        saludar();
-        saludar2();
+        mostrar_mensaje();
+    });
+
+    document.getElementById("mi_boton2").addEventListener("click", function() {
+        cambiar_color();
     });
 });
+
+function mostrar_mensaje() {
+    //declaracion de variables
+    var dato1;
+    var dato2;
+
+    //Asignacion de id a las variables
+    dato1 = document.getElementById("input1").value;
+    dato2 = document.getElementById("input2").value;
+
+    if (dato1.trim() ===""|| dato2.trim() ==="" )
+    {
+     alert("Por_favor_llene_sus_datos");
+    }
+    else
+    {
+        //construir mensaje
+        var mensaje = "Los datos son" +" " + dato1 +" "+ dato2;
+
+        //Mostrar mensaje
+        document.getElementById("resultado").innerText = mensaje;
+    }
+}
+function cambiar_color()
+{
+    var elemento = document.getElementById("contenedor");
+
+    if (document.body.style.backgroundColor === "black")
+        {
+            document.body.style.backgroundColor = "white";
+            elemento.style.width = "150px";
+            elemento.style.heigth = "150px";
+            elemento.style.backgroundColor="red";
+        }
+    else
+    {
+        document.body.style.backgroundColor = "black";
+        elemento.style.width = "200px";
+        elemento.style.heigth = "200px";
+        elemento.style.backgroundColor="blue";
+    }
+    
+}
