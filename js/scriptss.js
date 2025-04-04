1b1170d8b72057467bb759005c3138316dc7623b
LisbeniaGadea <lisbeniagad@gmail.com>
2025-04-03 20:41:29 -0600

    subir

--- js/scriptss.js
+++ js/scriptss.js
@@ -1,5 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownBtn = document.querySelector('.dropdown-button');
+    const dropdownContainer = document.querySelector('.dropdown-container');
+
+    dropdownBtn.addEventListener('click', () => {
+        dropdownContainer.classList.toggle('active');
+    });
+});
+
 function redirigir(pagina) {
     window.location.href = pagina;
 }
-
-
